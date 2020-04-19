f9bc99d0301e60b3f5059aa4dc3973eeeb88792f
ramazanyigit <54190298+ramazanyigit@users.noreply.github.com>
2020-04-19 23:35:19 +0300

    Adding multiple implementations of GreetingService to use qualifiers for dependency injection.

--- src/main/java/com/ramazanyigit/springdependencyinjectionexamples/controller/SetterInjectedController.java
+++ src/main/java/com/ramazanyigit/springdependencyinjectionexamples/controller/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.ramazanyigit.springdependencyinjectionexamples.controller;
 
 import com.ramazanyigit.springdependencyinjectionexamples.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ public class SetterInjectedController {
     private GreetingService greetingService;
 
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/test/java/com/ramazanyigit/springdependencyinjectionexamples/controller/SetterInjectedControllerTest.java
+++ src/test/java/com/ramazanyigit/springdependencyinjectionexamples/controller/SetterInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.ramazanyigit.springdependencyinjectionexamples.controller;
 
-import com.ramazanyigit.springdependencyinjectionexamples.service.GreetingServiceImpl;
+import com.ramazanyigit.springdependencyinjectionexamples.service.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterInjectedControllerTest {
 
@@ -13,7 +11,7 @@ class SetterInjectedControllerTest {
     @BeforeEach
     void setUp() {
         controller = new SetterInjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test
